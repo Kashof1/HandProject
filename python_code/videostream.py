0e56dcb184bb54382a8218eac57fec861dd06e91
Kashof1 <theibster6@gmail.com>
2022-12-24 19:07:53 +0000

    Edited videostream.py, it is now able to track and mark the hand and render this in real time

--- python_code/videostream.py
+++ python_code/videostream.py
@@ -1,1 +1,28 @@
 import cv2 as cv
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5) as hands:
+    capture = cv.VideoCapture(0)
+    while capture.isOpened():
+        val, frame = capture.read()
+
+        image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+        if results.multi_hand_landmarks:
+            for count, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
+
+        cv.imshow('Window', image)
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+
+
+capture.release()
+cv.destroyAllWindows()
