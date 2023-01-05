8295b44a2bfcbab15d79a8f4be677492bccd52f5
Kashof1 <theibster6@gmail.com>
2023-01-05 14:23:23 +0000

    installed black, ran black on videostream.py for code refactoring

--- python_code/videostream.py
+++ python_code/videostream.py
@@ -5,9 +5,11 @@ import mediapipe as mp
 def TrackedVideoFeed():
     mp_drawing = mp.solutions.drawing_utils
     mp_hands = mp.solutions.hands
-    with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5, max_num_hands = 3) as hands:
+    with mp_hands.Hands(
+        min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=3
+    ) as hands:
         capture = cv.VideoCapture(0)
-        width  = capture.get(3) 
+        width = capture.get(3)
         height = capture.get(4)
         while capture.isOpened():
             val, frame = capture.read()
@@ -21,29 +23,45 @@ def TrackedVideoFeed():
             if results.multi_hand_landmarks:
                 for count, hand in enumerate(results.multi_hand_landmarks):
                     mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
-                    print(results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].x)
-                    x = float(results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].x)
-                    y = float(results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].y)
+                    print(
+                        results.multi_hand_landmarks[0]
+                        .landmark[mp_hands.HandLandmark.WRIST]
+                        .x
+                    )
+                    x = float(
+                        results.multi_hand_landmarks[0]
+                        .landmark[mp_hands.HandLandmark.WRIST]
+                        .x
+                    )
+                    y = float(
+                        results.multi_hand_landmarks[0]
+                        .landmark[mp_hands.HandLandmark.WRIST]
+                        .y
+                    )
                     xwidth = x * width
                     xwidth = round(xwidth)
                     ywidth = y * height
                     ywidth = round(ywidth)
                     print(xwidth, ywidth)
-                    cv.putText(image, 
-                                (f"X: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].x}, Y: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].y}, Z: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].z}"), 
-                                (xwidth, ywidth),
-                                cv.FONT_HERSHEY_SIMPLEX, 
-                                0.5,
-                                (255, 255, 255),
-                                2)
+                    cv.putText(
+                        image,
+                        (
+                            f"X: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].x}, Y: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].y}, Z: {results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST].z}"
+                        ),
+                        (xwidth, ywidth),
+                        cv.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 255, 255),
+                        2,
+                    )
 
-            cv.imshow('Window', image)
-            if cv.waitKey(10) & 0xFF == ord('q'):
+            cv.imshow("Window", image)
+            if cv.waitKey(10) & 0xFF == ord("q"):
                 break
-
 
     capture.release()
     cv.destroyAllWindows()
+
 
 if __name__ == "__main__":
     TrackedVideoFeed()
